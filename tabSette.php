652b85955fee55933215765c3e1f4035f4107707
luca855 <64093237+luca855@users.noreply.github.com>
2020-04-21 19:01:48 +0200

    Add files via upload

---
+++ tabSette.php
@@ -0,0 +1,15 @@
+<html>
+
+    <body bgColor=#00FFFF>
+
+    <?php
+        
+        for($I=0; $I<=10; $I++){
+            $val=7*$I;
+            echo "<br>7 x $I = $val";
+        }
+
+    ?>
+    </body>
+
+</html>
